0471e6660fa4d63f6f00608839046d8d4c5d5e64
RyuRaseul skkusosc <ydh88283072@gmail.com>
2024-06-02 23:36:06 +0900

    Add code for testing before Quit

--- wordle.py
+++ wordle.py
@@ -9,5 +9,13 @@ size = [800, 600]
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Wordle")
 
+done = False
+
+while not done:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			done = True
+	pygame.display.flip()
+
 #Quit Code
 pygame.quit()
