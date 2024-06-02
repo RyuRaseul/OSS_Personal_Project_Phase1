34bdd87dace6bbbd5f7451d641abe8be75701e38
RyuRaseul skkusosc <ydh88283072@gmail.com>
2024-06-02 23:55:34 +0900

    Solving Non-Printable Character Error

--- wordle.py
+++ wordle.py
@@ -10,11 +10,11 @@ screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Wordle")
 
 #Color Var
-BLACK = (  0,   0,   0)
+BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-BLUE  = (  0,   0, 255)
-GREEN = (  0, 255,   0)
-RED   = (255,   0,   0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 Dimgray = (105, 105, 105)
 Darkgray = (169, 169, 169)
 
