ea9849c60454a96773d68b8560a4850f5537d1a7
RyuRaseul skkusosc <ydh88283072@gmail.com>
2024-06-02 23:25:52 +0900

    Setting Screen Size and Title

--- wordle.py
+++ wordle.py
@@ -4,5 +4,10 @@ from pygame.locals import *
 #Pygame Initailize
 pygame.init()
 
+#Screen Setting
+size = [800, 600]
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Wordle")
+
 #Quit Code
 pygame.quit()
