cebd2a5cc729ee0c317da746a1b4de7341fe1601
RyuRaseul skkusosc <ydh88283072@gmail.com>
2024-06-02 23:41:06 +0900

    Add Color code variable

--- wordle.py
+++ wordle.py
@@ -9,6 +9,15 @@ size = [800, 600]
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Wordle")
 
+#Color Var
+BLACK = (  0,   0,   0)
+WHITE = (255, 255, 255)
+BLUE  = (  0,   0, 255)
+GREEN = (  0, 255,   0)
+RED   = (255,   0,   0)
+Dimgray = (105, 105, 105)
+Darkgray = (169, 169, 169)
+
 done = False
 
 while not done:
