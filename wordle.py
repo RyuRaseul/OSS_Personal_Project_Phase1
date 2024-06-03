07ba44536e7ce85fb0f4e887fa262664b3c659d2
RyuRaseul skkusosc <ydh88283072@gmail.com>
2024-06-03 20:50:56 +0900

    Implement input text for first line

--- wordle.py
+++ wordle.py
@@ -28,12 +28,18 @@ tile_size = 80
 tile_spacing_x = 10
 tile_spacing_y = 10
 
+#Var for input_letter
+#current_x_pos = 120
+#current_y_pos = 70
+key_pressed = ""
+letter_font = pygame.font.SysFont("arial", 70, True, False)
+letter_size = 75
+
+
 #Var for guessing word
 total_guess = 0
 guess_word = []
 guess_word_string = ""
-key_pressed = ""
-
 
 class Tile:
 	def __init__(self, bg_color, x_pos, y_pos):
@@ -52,22 +58,36 @@ class Letter:
 		self.y = y_pos
 		self.letter = letter
 		self.text_surface = letter_font.render(self.letter, True, self.text_color)
-		self.text_rect = self.text_surface.get_rect(center=(x_pos +  tile_width/2, y_pos + tile_height/2))
+		self.text_rect = self.text_surface.get_rect(center=(x_pos + 40, y_pos + 40))
 	def draw(self):
-		pygame.draw.rect(screen, self.bg_color, [self.x, self.y, letter_size, letter_size], 4)
+		pygame.draw.rect(screen, self.bg_color, [self.x, self.y, tile_size, tile_size])
+		pygame.draw.rect(screen, BLACK, [self.x, self.y, tile_size, tile_size], 4)
 		screen.blit(self.text_surface, self.text_rect)
+
+def create_letter():
+	global default_x, guess_word_string
+	new_letter = Letter(key_pressed, default_x, default_y)
+	new_letter.draw()
+	default_x += tile_size + tile_spacing_x
+	guess_word_string += key_pressed
 
 #Creating Rect OBJ for word tile
 for i in range(6):
 	for j in range(5):
 		new_tile = Tile(BLACK, default_x + j*(tile_spacing_x + tile_size), default_y + i*(tile_spacing_y+ tile_size))
 		new_tile.draw()
+
 done = False
 
 while not done:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			done = True
+		elif event.type == pygame.KEYDOWN:
+			key_pressed = event.unicode.upper()
+			if key_pressed in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+				if len(guess_word_string) < 5:
+					create_letter()
 	pygame.display.flip()
 
 
